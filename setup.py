5fcbe2675a709e384e50f8bc67634a7d04a06beb
Serial Lab <slab@serial-lab.com>
2018-10-11 15:10:30 -0600

    Bump version: 0.1.3 → 0.1.4

--- setup.py
+++ setup.py
@@ -44,6 +44,6 @@ setup(
     test_suite='tests',
     tests_require=test_requirements,
     url='https://gitlab.com/serial-lab/gs123',
-    version='0.1.3',
+    version='0.1.4',
     zip_safe=False,
 )
