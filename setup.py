8760f4ba1791f3a5ff0c24bfc095bce21fd3bfa3
Serial Lab <slab@serial-lab.com>
2018-10-26 11:51:29 -0600

    updated the version number

--- setup.py
+++ setup.py
@@ -44,6 +44,6 @@ setup(
     test_suite='tests',
     tests_require=test_requirements,
     url='https://gitlab.com/serial-lab/gs123',
-    version='1.0.3',
+    version='1.0.0',
     zip_safe=False,
 )
