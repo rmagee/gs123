21382284600dec5999652a4d74915b4dd07416d3
Serial Lab <slab@serial-lab.com>
2018-10-26 12:07:39 -0600

    update version ahead of PyPi

--- setup.py
+++ setup.py
@@ -44,6 +44,6 @@ setup(
     test_suite='tests',
     tests_require=test_requirements,
     url='https://gitlab.com/serial-lab/gs123',
-    version='1.0.1',
+    version='1.0.4',
     zip_safe=False,
 )
