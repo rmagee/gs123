2a51d5ed1b23be2c6ca10822e2a483e404f5f068
Chuck Sailer <chuck@chucksailer.com>
2019-05-23 17:18:48 -0400

    updated version

--- setup.py
+++ setup.py
@@ -44,6 +44,6 @@ setup(
     test_suite='tests',
     tests_require=test_requirements,
     url='https://gitlab.com/serial-lab/gs123',
-    version='1.0.6',
+    version='1.0.7',
     zip_safe=False,
 )
